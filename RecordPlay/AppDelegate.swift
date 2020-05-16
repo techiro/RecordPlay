393b553e8e724881b205c8f9d0a84cc91c235f65
techiro <techiromen0@gmail.com>
2020-05-16 13:49:36 +0900

    Initial Commit

---
+++ RecordPlay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RecordPlay
+//
+//  Created by TanakaHirokazu on 2020/05/16.
+//  Copyright © 2020 TanakaHirokazu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RecordPlay/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RecordPlay
+//
+//  Created by TanakaHirokazu on 2020/05/16.
+//  Copyright © 2020 TanakaHirokazu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
